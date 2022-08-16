e2112ea84cb181fdd303411b6770bfe36e2cada1
ravneet998 <109993408+ravneet998@users.noreply.github.com>
2022-08-16 18:57:31 -0400

    Update home.js

--- public/src/home.js
+++ public/src/home.js
@@ -1,15 +1,15 @@
 function getTotalBooksCount(books) {
-  return books.length;
+  return books.length; // .length returns the number of books in the array
 }
 
 function getTotalAccountsCount(accounts) {
-  return accounts.length;
+  return accounts.length; // .length returns the number of accounts in the array
 }
 
 function getBooksBorrowedCount(books) {
-  return books.reduce((borrowCount, { borrows }) => {
+  return books.reduce((borrowCount, { borrows }) => { // .reduce method added to borrowed books, and is used to iterate through each book in array.
     const mostRecent = borrows[0];
-    if (!mostRecent.returned) borrowCount++;
+    if (!mostRecent.returned) borrowCount++; // if most recent transaction of borrows have not been returned, the total borrow counter is increased.
     return borrowCount;
   }, 0);
 }
@@ -31,16 +31,16 @@ function getMostCommonGenres(books) {
    };
   })
   .sort((a, b) => b.count - a.count)
-  .slice(0, 5);
+  .slice(0, 5); // returns an array containing five objects or fewer that represents the most common occurring genres, ordered from most common to least.
 }
 
 function getMostPopularBooks(books) {
   return books
-  .map((book) => {
+  .map((book) => { // map through each book in books
    return { name: book.title, count: book.borrows.length };
   })
   .sort((a, b) => (a.count < b.count ? 1 : -1))
-  .slice(0, 5);
+  .slice(0, 5); // returns an array containing five objects or fewer that represents the most popular books in the library
 }
 
 function getMostPopularAuthors(books, authors) {
@@ -57,7 +57,7 @@ function getMostPopularAuthors(books, authors) {
   });
   result.push(theAuthor);
  });
- return result.sort((a, b) => b.count - a.count).slice(0, 5);
+ return result.sort((a, b) => b.count - a.count).slice(0, 5); // returns an array containing five objects or fewer that represents the most popular authors whose books have been checked out the most
 }
 
 module.exports = {
