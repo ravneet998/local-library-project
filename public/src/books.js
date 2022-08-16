c6d70d3da31318e5b2b3ad2a1b7fd886241791dc
ravneet998 <109993408+ravneet998@users.noreply.github.com>
2022-08-16 18:46:38 -0400

    Update books.js

--- public/src/books.js
+++ public/src/books.js
@@ -1,27 +1,27 @@
 function findAuthorById(authors, id) {
-  let found = authors.find((author) => author.id === id);
-  return found;
+  let found = authors.find((author) => author.id === id); // .find to search for matching author id
+  return found; // returns the author object that has the matching id
 }
 
 function findBookById(books, id) {
-  let foundBooks = books.find((book) => book.id === id);
-  return foundBooks
+  let foundBooks = books.find((book) => book.id === id); // .find to search for matching book id
+  return foundBooks // returns the book object that has the matching id
 }
 
-function partitionBooksByBorrowedStatus(books) {
+function partitionBooksByBorrowedStatus(books) { // returns an array with two arrays inside of it. all of the inputted books are present in either the first or second array.
   let booksReturned = books.filter((book) =>
-  book.borrows.every((borrow) => borrow.returned === true)
+  book.borrows.every((borrow) => borrow.returned === true) // book objects representing books that have been returned
  );
   
   let booksBorrowed = books.filter((book) =>
-  book.borrows.some((borrow) => borrow.returned === false)
+  book.borrows.some((borrow) => borrow.returned === false) // book objects representing books that are currently checked out
  );
   
   let lastArray = [[...booksBorrowed], [...booksReturned]];
  return lastArray;
 }
 
-function getBorrowersForBook(book, accounts) {
+function getBorrowersForBook(book, accounts) { 
 return book.borrows
   .map((borrow) => {
    let account = accounts.find((account) => account.id === borrow.id);
@@ -29,7 +29,7 @@ return book.borrows
   })
   .slice(0, 10);
 }
-
+// return an array of ten or fewer account objects that represents the accounts given by the IDs in the provided book's `borrows` array. However, each account object should include the `returned` entry from the corresponding transaction object in the `borrows` array
 module.exports = {
   findAuthorById,
   findBookById,
